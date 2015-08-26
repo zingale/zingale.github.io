addf8a5edc831d51d624024591683311aa1f9659
Michael Zingale <michael.zingale@stonybrook.edu>
2015-08-25 20:38:14 -0400

    write out the html

--- paper-sorter/write_pubhtml.py
+++ paper-sorter/write_pubhtml.py
@@ -3,14 +3,25 @@ import parser
 papers = parser.parse_bibfile("papers.bib")
 
 current_year = 3000
+first = True
 
 for p in papers:
     if p.year < current_year:
-        print "\n{}\n".format(p.year)
+        if not first:
+            print "</dl>\n"
+        else:
+            first = False
+
+        print "<p><h2>{}</h2>\n".format(p.year)
+        print "<dl>\n"
+
         current_year = p.year
 
     t, o, l = p.jstring()
-    print "{}\n{}\n{}\n".format(t, o, l)
+    print "<dt>{} <a href='{}'>[link]</a></dt>\n".format(t, l)
+    print "<dd>{}</dd>\n".format(o)
+
+
 
 
 
